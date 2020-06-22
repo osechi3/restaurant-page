2f0b4f5776f2d49fb969334aac5f25b203624b1b
osechi3 <holypoetwrk@gmail.com>
2020-06-22 22:18:37 +0300

    Creating donuts object

--- src/menuPageLoad.js
+++ src/menuPageLoad.js
@@ -1,5 +1,54 @@
 const menuPage = () => {
 
+    const donuts = [
+        {
+            name: 'Banana Donut',
+            image: '/img/banana-donut.jpg',
+            description: 'test'
+        },
+        {
+            name: 'Banana Protein Donut',
+            image: '/img/banana-protein-donut.jpg',
+            description: 'test'
+        },
+        {
+            name: 'Caramel Donut',
+            image: '/img/caramel-donut.jpg',
+            description: 'test'
+        },
+        {
+            name: 'Chocolate Donut',
+            image: '/img/chocolate-donut.jpg',
+            description: 'test'
+        },
+        {
+            name: 'Double-Bouble Donut',
+            image: '/img/double-bouble-donut.jpg',
+            description: 'test'
+        },
+        {
+            name: 'Protein Donut',
+            image: '/img/protein-donut.jpg', 
+            description: 'test'
+        },
+        {
+            name: 'Rainbow Donut',
+            image: '/img/rainbow-donut.jpg',
+            description: 'test'
+        },
+        {
+            name: 'Strawberry Donut',
+            image: '/img/strawberry-donut.jpg',
+            description: 'test'
+        },
+        {
+            name: 'Bitten Donut :(',
+            image: '/img/bitten-donut.jpg',
+            description: 'test'
+        },
+    ];
+        
+    
     const tabContent = document.querySelector('#tab-content');
 
     const createMenuContainer = () => {
@@ -14,7 +63,7 @@ const menuPage = () => {
                 menuItem.setAttribute('data-order',`${menuContainer.childNodes.length}`);
 
                 const img = document.createElement('img');
-                img.setAttribute('src', `https://healthyfitnessmeals.com/wp-content/uploads/2018/06/instagram-In-Stream_Square___banana-protein-donuts-3.jpg`);
+                img.setAttribute('src', `/img/banana-protein-donut.jpg`);
                 img.classList.add('items-img');
 
                 menuItem.appendChild(img);
@@ -23,6 +72,7 @@ const menuPage = () => {
         }
 
         const createImg = () => {
+
             // put src adresses in an array and make a loop assigning each link to a new img created
         }
 
