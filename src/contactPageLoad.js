7646a57c4bff23e034f937be316a48e6eab471f3
osechi3 <holypoetwrk@gmail.com>
2020-06-23 22:09:11 +0300

    Adding Contact page

---
+++ src/contactPageLoad.js
@@ -0,0 +1,37 @@
+const contactPage = () => {
+
+    const tabContent = document.querySelector('#tab-content');
+
+    const title = document.createElement('h1');
+    const infoContainer = document.createElement('div');
+
+    title.textContent = 'Contact Us!';
+    title.setAttribute('id', 'contact-title');
+    infoContainer.setAttribute('id', 'info-container');
+
+    tabContent.appendChild(title);
+    tabContent.appendChild(infoContainer);
+
+    const contactPageContent = () => {
+        const address = document.createElement('div');
+        const email = document.createElement('div');
+        const phone = document.createElement('div');
+        const contactImg = document.createElement('img');
+
+        address.style.cssText = 'flex-basis: 100%; padding-bottom: 25px;'; // moving the other 2 elements onto the next row
+        contactImg.setAttribute('id', 'contact-img');
+        contactImg.setAttribute('src', `https://media3.s-nbcnews.com/j/newscms/2019_11/2781946/190311-billy-by-donut-shop-cs-347p_fd4688e43a8514c2fae22ffceeb2e9a1.social_share_1024x768_scale.jpg`);
+        contactImg.setAttribute('alt', 'Shop owners (not really)');
+
+        address.innerHTML = `<span style="font-size: 34px;">Adress:</span></br> 2977  Johnny Lane, Milwaukee, Wisconsin`;
+        email.innerHTML = `<span style="font-size: 34px;">Email:</span></br> donut-shop@gmail.com`;
+        phone.innerHTML = `<span style="font-size: 34px;">Phone:</span></br> +09531257688`;
+
+        infoContainer.appendChild(address);
+        infoContainer.appendChild(email);
+        infoContainer.appendChild(phone);
+        infoContainer.appendChild(contactImg);
+    }
+    contactPageContent();
+}
+export {contactPage};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import {mainPage, mainPageContent} from './mainPageLoad'
 import{menuPage} from './menuPageLoad';
+import{contactPage} from './contactPageLoad';
 mainPage();
 
 const navElement1 = document.querySelector('#nav-element1');
@@ -13,6 +14,12 @@ header.addEventListener('click', () => {
     resetTabContent();
     mainPageContent();
 })
+
+const navElement2 = document.querySelector('#nav-element2');
+navElement2.addEventListener('click', () => {
+    resetTabContent();
+    contactPage();
+});
 
 function resetTabContent() {
     const tabContent = document.querySelector('#tab-content');
