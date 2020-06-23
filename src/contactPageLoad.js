10c1c0ce12da3e28211ef1260cc9cdf80f2eb609
osechi3 <holypoetwrk@gmail.com>
2020-06-23 22:47:41 +0300

    Fixing a typo and a margin on the contact page

--- src/contactPageLoad.js
+++ src/contactPageLoad.js
@@ -23,7 +23,7 @@ const contactPage = () => {
         contactImg.setAttribute('src', `https://media3.s-nbcnews.com/j/newscms/2019_11/2781946/190311-billy-by-donut-shop-cs-347p_fd4688e43a8514c2fae22ffceeb2e9a1.social_share_1024x768_scale.jpg`);
         contactImg.setAttribute('alt', 'Shop owners (not really)');
 
-        address.innerHTML = `<span style="font-size: 34px;">Adress:</span></br> 2977  Johnny Lane, Milwaukee, Wisconsin`;
+        address.innerHTML = `<span style="font-size: 34px;">Address:</span></br> 2977  Johnny Lane, Milwaukee, Wisconsin`;
         email.innerHTML = `<span style="font-size: 34px;">Email:</span></br> donut-shop@gmail.com`;
         phone.innerHTML = `<span style="font-size: 34px;">Phone:</span></br> +09531257688`;
 
